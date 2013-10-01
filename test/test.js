f546f572841e4929053752c14c3fe7f1d4e5428e
Charlotte Gore <conspiracygore@gmail.com>
2013-10-01 14:32:29 +0100

    updated

--- test/test.js
+++ test/test.js
@@ -1,1 +1,13 @@
-// Insert tests here.
+
+describe("suite", function(){
+
+	it("is ready to go for tests", function(){
+
+		should.exist(describe);
+		should.exist(it);
+		should.exist(should);
+		should.exist(expect);
+
+	})
+
+})
